716ab9f6e91d60fec45ac4bd7b955c884a353843
dear_weronika <weronika.harlos@gmail.com>
2022-11-18 20:13:51 +0000

    add icon and new tab

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
-import requests 
+import requests
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/city')
-def search_city():
+@app.route('/')
+def today():
     API_KEY = '76e279b59efb88310df280e9c8142315' # initialize your key here
     # city = request.args.get('q')  # city name passed as argument
     lat = 55.95
@@ -16,17 +16,17 @@ def search_city():
     print("Calling API:")
 
     # call API and convert response into Python dictionary
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=55.95&lon=-03.18&appid=76e279b59efb88310df280e9c8142315&units=metric"
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=76e279b59efb88310df280e9c8142315&units=metric'
     
     apiReq = requests.get(url)
     
     data = apiReq.json()
     def time_converter(unix):
-        return datetime.utcfromtimestamp(unix).strftime(' %H:%M:%S')
+        return datetime.utcfromtimestamp(unix).strftime(' %H:%M')
     rise = data["sys"]["sunrise"]  
     set = data["sys"]["sunset"]
     name = data["name"]
-    icon = data["weather"][0]["icon"]
+    icon = f'http://openweathermap.org/img/wn/{data["weather"][0]["icon"]}@2x.png'
     description = data["weather"][0]["description"]
     temp = round(data["main"]["temp"])
     sunrise = time_converter(rise)
@@ -35,28 +35,72 @@ def search_city():
     temp_min = round(data["main"]["temp_min"])
     
     
-    # return data
-    
     return render_template("index.html", name = name, icon = icon, description = description,temp = temp, sunrise = sunrise, sunset = sunset, temp_max = temp_max, temp_min = temp_min)
 
-    # # error like unknown city name, inavalid api key
-    # if response.get('cod') != 200:
-    #     message = response.get('message', '')
-    #     return f'Error getting temperature for {city.title()}. Error message = {message}'
 
-    # # get current temperature and convert it into Celsius
-    # current_temperature = response.get('main', {}).get('temp')
-    # if current_temperature:
-    #     current_temperature_celsius = round(current_temperature - 273.15, 2)
-    #     return f'Current temperature of {city.title()} is {current_temperature_celsius} &#8451;'
-    # else:
-    #     return f'Error getting temperature for {city.title()}'
+@app.route('/5day')
+def fiveDay():
+    API_KEY = '76e279b59efb88310df280e9c8142315' # initialize your key here
+    # city = request.args.get('q')  # city name passed as argument
+    lat = 55.95
+    lon = -3.18
+    
+    print("Calling API:")
 
+    # call API and convert response into Python dictionary
+    url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=76e279b59efb88310df280e9c8142315&units=metric'
+    
+    apiReq = requests.get(url)
+    
+    data = apiReq.json()
+    
+    name = data["city"]["name"]
+    icon0 = data["list"][0]["weather"][0]["icon"]
+    # icon = f'http://openweathermap.org/img/wn/{data["weather"][0]["icon"]}@2x.png'
+    description = data ["list"][0]["weather"][0]["description"]
+    temp = round(data["list"][0]["main"]["temp"])
+    # sunrise = time_converter(rise)
+    # sunset = time_converter(set)
+    # temp_max = round(data["main"]["temp_max"])
+    # temp_min = round(data["main"]["temp_min"])
+    data['list']
+    # def midDay(day):
+    #     if day["dt_txt"] == "2022-11-18 00:00:00":
+    #         return day
+    # return midDay(data['list'])
+    return data['list']
+    # return render_template("index.html", name = name, icon = icon, description = description,temp = temp, sunrise = sunrise, sunset = sunset, temp_max = temp_max, temp_min = temp_min)
 
-@app.route('/')
-def index():
- 
-    return '<h1>Weather</h1>'
+@app.route('/alert')
+def alert():
+    API_KEY = '76e279b59efb88310df280e9c8142315' # initialize your key here
+    # city = request.args.get('q')  # city name passed as argument
+    lat = 55.95
+    lon = -3.18
+    
+    print("Calling API:")
+
+    # call API and convert response into Python dictionary
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=76e279b59efb88310df280e9c8142315&units=metric'
+    
+    apiReq = requests.get(url)
+    
+    data = apiReq.json()
+    def time_converter(unix):
+        return datetime.utcfromtimestamp(unix).strftime(' %H:%M')
+    rise = data["sys"]["sunrise"]  
+    set = data["sys"]["sunset"]
+    name = data["name"]
+    icon = f'http://openweathermap.org/img/wn/{data["weather"][0]["icon"]}@2x.png'
+    description = data["weather"][0]["description"]
+    temp = round(data["main"]["temp"])
+    sunrise = time_converter(rise)
+    sunset = time_converter(set)
+    temp_max = round(data["main"]["temp_max"])
+    temp_min = round(data["main"]["temp_min"])
+    
+    
+    return render_template("index.html", name = name, icon = icon, description = description,temp = temp, sunrise = sunrise, sunset = sunset, temp_max = temp_max, temp_min = temp_min)
 
 
 if __name__ == '__main__':
