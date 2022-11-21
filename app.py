c53c0c4a4fb349e78ab28a6ef9a2dfac95950629
dear_weronika <weronika.harlos@gmail.com>
2022-11-21 20:50:22 +0000

    Added five day forecast API

--- app.py
+++ app.py
@@ -51,24 +51,26 @@ def fiveDay():
     url = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=76e279b59efb88310df280e9c8142315&units=metric'
     
     apiReq = requests.get(url)
-    
+    # Change API format to Json
     data = apiReq.json()
+ 
+    # Json -> Daytime -> string (hour)
+    def midDay(day):
+        return datetime.fromisoformat(day["dt_txt"]).strftime("%H")=="12"
+    
+    # filter elements from data["list"] list that dt_txt property is = 12
+    fiveDayForecast = [d for d in data["list"] if midDay(d)]
+    
     
     name = data["city"]["name"]
     icon0 = data["list"][0]["weather"][0]["icon"]
     # icon = f'http://openweathermap.org/img/wn/{data["weather"][0]["icon"]}@2x.png'
     description = data ["list"][0]["weather"][0]["description"]
     temp = round(data["list"][0]["main"]["temp"])
-    # sunrise = time_converter(rise)
-    # sunset = time_converter(set)
-    # temp_max = round(data["main"]["temp_max"])
-    # temp_min = round(data["main"]["temp_min"])
-    data['list']
-    # def midDay(day):
-    #     if day["dt_txt"] == "2022-11-18 00:00:00":
     #         return day
-    # return midDay(data['list'])
-    return data['list']
+    
+    # return render_template("fiveday.html", hour = weatherTime)
+    return fiveDayForecast
     # return render_template("index.html", name = name, icon = icon, description = description,temp = temp, sunrise = sunrise, sunset = sunset, temp_max = temp_max, temp_min = temp_min)
 
 @app.route('/alert')
