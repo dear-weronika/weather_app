a468fe14c418185feb895292b30e604fca23c795
dear_weronika <weronika.harlos@gmail.com>
2022-12-08 00:30:03 +0000

    add edinburgh to route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/edinburgh')
 def today():
     todayWeather = helpers.getWeatherData()
     rise = todayWeather["sys"]["sunrise"]  
